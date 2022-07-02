be66ceec6dedbc23c41c4ab1802bef689263d709
Quy <quyseo.ictu@gmail.com>
2022-07-02 15:26:12 +0700

    access user

--- custom_b2c/models/res_users.py
+++ custom_b2c/models/res_users.py
@@ -9,4 +9,6 @@ class ResUsers(models.Model):
 
     user_profile = fields.Many2one(comodel_name='user.profile', string='Tài khoản giao dịch', default=None)
     partner = fields.Many2one(comodel_name='partner', string='Cửa hàng đối tác')
-    thuoc_kho = fields.Many2many(comodel_name='nha.kho', string='Nhà kho')
+    thuoc_kho = fields.Many2one(comodel_name='nha.kho', string='Nhà kho')
+    sua_chua = fields.Many2many(comodel_name='sua.chua', string='Công việc sửa chữa')
+    cong_viec = fields.Many2many(comodel_name='cong.viec', string='Công việc của tôi')
